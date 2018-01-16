d380b0e6b3d09002589ad220b9de170366e9f1dc
PythonLimited <leonardomeitz@gmail.com>
2018-01-16 20:18:48 +0100

    los 15.1 bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,8 +26,8 @@ AUDIO_FEATURE_SAMSUNG_DUAL_SIM := true
 
 # Radio
 SIM_COUNT := 2
-TARGET_GLOBAL_CFLAGS += -DANDROID_MULTI_SIM
-TARGET_GLOBAL_CPPFLAGS += -DANDROID_MULTI_SIM
+LOCAL_CFLAGS += -DANDROID_MULTI_SIM
+LOCAL_CPPFLAGS += -DANDROID_MULTI_SIM
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
@@ -42,6 +42,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37  androidboot.selinux=permissive androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
 TARGET_KERNEL_SOURCE := kernel/samsung/s3ve3g
 TARGET_KERNEL_CONFIG := lineageos_s3ve3g_defconfig
